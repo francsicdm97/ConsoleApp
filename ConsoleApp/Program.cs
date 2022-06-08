67128db79882c78258e67f0f35daeac7073589a0
francis mcdermott <francismcd97@gmail.com>
2022-06-08 15:14:08 +0100

    Parsing of strings

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,8 +5,44 @@ namespace ConsoleApp
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+         {
+             //implicit conversion
+
+             int num = 123905322;
+             long bignum = num;
+
+             float myFloat = 13.37f;
+             double myNewdouble = myFloat;
+
+             double myDouble = 13.37;
+             int myInt;
+
+             //explicit conversion
+             //cast double to int
+             myInt = (int)myDouble;
+
+             //type conversion
+             string myDoubleString = myDouble.ToString(); //"13.37"
+             string myFloatString = myFloat.ToString();
+
+             bool sunIsShining = true;
+             string myBoolString = sunIsShining.ToString();
+
+             string myString = "15";
+             string myString2 = "13";
+             int num1 = Int32.Parse(myString);
+             int num2 = Int32.Parse(myString2);
+             int resultInt = num1 + num2;
+             string result = myString + myString2;
+
+
+             Console.WriteLine(resultInt);
+             Console.ReadKey();
+         } 
+
+
+       
+
     }
 }
+
